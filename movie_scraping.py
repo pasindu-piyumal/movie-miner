5edb5fcb9bf19e4350b533fd7e48461d37875e4f
pasindu-piyumal <22cds0423@ms.sab.ac.lk>
2026-04-03 10:07:31 +0530

    setup driver

--- movie_scraping.py
+++ movie_scraping.py
@@ -5,3 +5,11 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
+
+options = webdriver.ChromeOptions()
+options.add_argument('--start-maximized')
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
